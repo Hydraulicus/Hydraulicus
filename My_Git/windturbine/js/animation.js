b83ed53b51f13cea407a72af4d6d41a968b9c8d7
zbbrn <alexei.zababurin@gmail.com>
2016-04-13 14:21:31 +0300

    Add new vertion

---
+++ My_Git/windturbine/js/animation.js
@@ -0,0 +1,127 @@
+ var json = [{"name":"WSSC Wind Use", "vol":22},
+				{"name":"US Wind Generation", "vol":4.7}],//data fot infographic
+	 iLook = {R : 300, r : 100 //R - out radius in px, r - inner radius
+			 ,color : 'hsl(240,50%,75%)' //color of outer data circle
+			},
+// angles = [{'x':0,'dx':30},{'x':0,'dx':5}];
+angles = [20,5];
+// console.log(json);
+
+var width = 800,
+    height = width,
+    radius = Math.min(width, height) / 2;
+
+    tau = 2 * Math.PI; 
+
+
+var arc = [
+    d3.svg.arc().innerRadius(180).outerRadius(240).startAngle(0),
+    d3.svg.arc().innerRadius(120).outerRadius(180).startAngle(0),
+    ];
+
+
+var svg = d3.select("#animation").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+
+
+// Add the background arc, from 0 to 100% .
+for (var i in json) {
+        var background = svg.append("path")
+            .datum({endAngle: tau})
+            .style("fill", "#eee")
+            .attr({"stroke" : "#555", "stroke-width" : 1})
+            .attr("d", arc[i]);
+}
+
+
+var foreground = [
+    svg.append("path")
+    .datum({endAngle: 0})
+    .style("fill", iLook.color)
+    .attr({"stroke" : "#555", "stroke-width" : 1})
+    .attr("d", arc[0]),
+
+    svg.append("path")
+    .datum({endAngle: 0})
+    .style("fill", iLook.color)
+    .attr({"stroke" : "#555", "stroke-width" : 1})
+    .attr("d", arc[1]),
+    ];
+
+for (var i in json) {
+   console.log(i,json[i].vol);};
+
+// Every so often, start a transition to a new random angle. Use transition.call
+// (identical to selection.call) so that we can encapsulate the logic for
+// tweening the arc in a separate function below.
+setTimeout(function() {
+var EndAngle = json[0].vol * 0.01 * tau;
+  foreground[0].transition()
+      .duration(750)
+      .call(arcTween, EndAngle, 0);
+
+ EndAngle = json[1].vol * 0.01 * tau;
+    foreground[1].transition()
+      .duration(750)
+      .call(arcTween, EndAngle, 1);
+
+
+}, 500);
+
+// svg.transition()
+//       .duration(1050)
+//       .attr("transform", "translate(" + width / 4 + "," + height / 4 + ")");
+
+
+// Creates a tween on the specified transition's "d" attribute, transitioning
+// any selected arcs from their current angle to the specified new angle.
+function arcTween(transition, newAngle, i) {
+
+  // The function passed to attrTween is invoked for each selected element when
+  // the transition starts, and for each element returns the interpolator to use
+  // over the course of transition. This function is thus responsible for
+  // determining the starting angle of the transition (which is pulled from the
+  // element's bound datum, d.endAngle), and the ending angle (simply the
+  // newAngle argument to the enclosing function).
+  transition.attrTween("d", function(d) {
+
+    // To interpolate between the two angles, we use the default d3.interpolate.
+    // (Internally, this maps to d3.interpolateNumber, since both of the
+    // arguments to d3.interpolate are numbers.) The returned function takes a
+    // single argument t and returns a number between the starting angle and the
+    // ending angle. When t = 0, it returns d.endAngle; when t = 1, it returns
+    // newAngle; and for 0 < t < 1 it returns an angle in-between.
+    var interpolate = d3.interpolate(d.endAngle, newAngle);
+
+    // The return value of the attrTween is also a function: the function that
+    // we want to run for each tick of the transition. Because we used
+    // attrTween("d"), the return value of this last function will be set to the
+    // "d" attribute at every tick. (It's also possible to use transition.tween
+    // to run arbitrary code for every tick, say if you want to set multiple
+    // attributes from a single function.) The argument t ranges from 0, at the
+    // start of the transition, to 1, at the end.
+    return function(t) {
+
+      // Calculate the current arc angle based on the transition time, t. Since
+      // the t for the transition and the t for the interpolate both range from
+      // 0 to 1, we can pass t directly to the interpolator.
+      //
+      // Note that the interpolated angle is written into the element's bound
+      // data object! This is important: it means that if the transition were
+      // interrupted, the data bound to the element would still be consistent
+      // with its appearance. Whenever we start a new arc transition, the
+      // correct starting angle can be inferred from the data.
+      d.endAngle = interpolate(t);
+
+      // Lastly, compute the arc path given the updated data! In effect, this
+      // transition uses data-space interpolation: the data is interpolated
+      // (that is, the end angle) rather than the path string itself.
+      // Interpolating the angles in polar coordinates, rather than the raw path
+      // string, produces valid intermediate arcs during the transition.
+      return arc[i](d);
+    };
+  });
+}
