625510617317e0025539d16c14560b8ae3a849d3
zbbrn <alexei.zababurin@gmail.com>
2016-04-15 20:59:31 +0300

    Add new vertion

--- My_Git/windturbine/js/animation.js
+++ My_Git/windturbine/js/animation.js
@@ -1,6 +1,6 @@
  var json = [{"name":"WSSC Wind Use", "vol":22},
 				{"name":"US Wind Generation", "vol":4.7}],//data fot infographic
-	 iLook = {R : 180, w : 60 //R - out radius in px, w - width
+	 iLook = {R : 315, w : 105 //R - out radius in px, w - width
 			 ,color : '#6D9EEB' //color of outer data circle
 			};
 
@@ -11,8 +11,16 @@ var textes = [   {'text' : json[0].name, 'x' : 100, 'y' : -255}
                 ,{'text' : '('+json[1].vol+')', 'x':-120, 'y' : -230, 'endX' : 15, 'endY' : -90}
                 ];
 
-var width = 800,
-    height = width,
+var turbine = [ {d : "M428 417c-4,-9 3,-15 8,-14 33,20 112,64 143,86l111 73c2,1 0,4 -2,3 -109,-60 -220,-105 -239,-132l-21 -16z"}
+                ,{d : "M425 392c-6,7 -15,5 -16,-1 1,-38 -1,-129 2,-166l8 -133c1,-2 4,-2 4,0 3,125 20,243 5,273l-3 27z"}
+                ,{d : "M404 406c10,2 12,11 8,15 -34,19 -111,65 -145,81l-119 59c-2,1 -4,-2 -2,-3 107,-65 201,-138 234,-141l24 -11z"}
+                ,{d : "M426 389c6,2 10,8 10,15l0 -1c-5,-1 -12,5 -8,14l2 2c-3,2 -7,4 -11,4 -3,0 -5,-1 -7,-2l0 0c4,-4 2,-13 -8,-15l-2 2c0,-1 0,-2 0,-3 0,-6 3,-11 7,-14l0 0c1,6 10,8 16,1l1 -3z"}
+                // ,{d : "M419 426c-4,0 -11,0 -14,-2 -4,-12 -7,-21 -6,-33 1,-4 4,-5 20,-5m0 40c5,0 12,0 15,-2 4,-12 6,-21 5,-33 0,-4 -4,-5 -20,-5"}
+
+                ]
+
+var width = 836,
+    height = 1258,
     radius = Math.min(width, height) / 2;
     tau = 2 * Math.PI; 
 
@@ -26,8 +34,11 @@ var arc = [
 var svg = d3.select("#animation").append("svg")
     .attr("width", width)
     .attr("height", height)
+     .attr("class", "graph-svg-component")
+    // .style("border","2px solid '#555'")
+    // .style("background","linear-gradient(to top, #bbb, white);")
     .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    // .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
 
 // Define the gradient
 var gradient = svg.append("svg:defs")
@@ -55,6 +66,7 @@ gradient.append("svg:stop")
 // Add the background arc, from 0 to 100% .
 for (var i in json) {
         var background = svg.append("path")
+            .attr("transform", "translate(418,406)")
             .datum({endAngle: tau})
             .style("fill", "#efefef")
             .attr({"stroke" : "#888", "stroke-width" : 1, "opacity" : 0.1})
@@ -65,18 +77,36 @@ for (var i in json) {
             .attr({"opacity" : 1});
 }
 
+// Add the windturbine 
+for (var i in turbine) {
+        var background = svg.append("path")
+            // .style("fill", "#555")
+            .attr({"stroke" : "#888", "stroke-width" : 2, "fill" : "#555", "opacity" : 1})
+            .classed("turbine", true)
+            // .attr("transform", "translate(-420,-406)")
+            .attr("d", turbine[i].d)
+            .transition()
+            .duration(4500)
+//
+            .attrTween("transform", tween)
+             ;
+   
+    function tween(d, i, a) { return d3.interpolateString("rotate(0, 418,406)", "rotate(360, 418,406)");};
+}
 
 var foreground = [
     svg.append("path")
     .datum({endAngle: 0})
     .style("fill", 'url(#gradient)')
     .attr({"stroke" : "#888", "stroke-width" : 0})
+    .attr("transform", "translate(418,406)")
     .classed("partition", true)
     .attr("d", arc[0]),
 
     svg.append("path")
     .datum({endAngle: 0})
     .style("fill", 'url(#gradient)')
+    .attr("transform", "translate(418,406)")
     .attr({"stroke" : "#888", "stroke-width" : 0, opacity : 0.65})
     .classed("partition", true)
     .attr("d", arc[1]),
@@ -92,7 +122,7 @@ setTimeout(function() {
 var EndAngle = json[0].vol * 0.01 * tau;
   foreground[0].transition()
       .duration(750)
-      // .delay(1000)
+      .delay(1000)
       .call(arcTween, EndAngle, 0);
 
  EndAngle = json[1].vol * 0.01 * tau;
@@ -131,7 +161,8 @@ var textLabels = texts
                 .duration(2000)
                 .attr({"opacity" : 1, "font-size" : "20px"});
 // debugger;
-var racks = svg.selectAll('path:not(.partition)')
+var racks = svg.selectAll('path .dash')
+// .selectAll('path:not(.partition)')
                 .data(textes)
                 .enter()
                 .append("path");
