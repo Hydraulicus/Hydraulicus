96086b19cba10cfd01531acd831db77e3646e801
zbbrn <852gin@gmail.com>
2016-06-15 20:39:01 +0300

    Add new vertion

--- My_Git/electricsky/js/animation.js
+++ My_Git/electricsky/js/animation.js
@@ -1,12 +1,14 @@
 var rhythm = 50 //ms,
 ,firsttiming = 200
 ,tubeColor = 'rgba(180,205,250,0.85)'
+,intermidColor = 'rgba(120,155,170,0.85)'
 ,darckTube = 'rgba(10, 10, 10,0.15)'
 ;
  
 var tubes = []
   , animSvg
-  , glow;
+  , glow
+  , filterChild;
 
 
 
@@ -15,7 +17,8 @@ function initAnimation (obj) {
     rhythm = obj.rhythm * 1000;
     firsttiming = obj.firsttiming * 1000;
 
-    glow = animSvg.filter(Snap.filter.shadow(0, 4, 4, '#99f'));
+    glow = animSvg.filter(Snap.filter.shadow(0, 4, 4, '#99f', 1));
+
 var deskpattern = animSvg.line(0, 10, 0, 0).attr({
         fill: "none",
         stroke: "#333",
@@ -28,34 +31,30 @@ Snap.select('#signdesk').attr({fill : deskpattern})
 
 };//end of init function
 
-// function lightTube(idtubeN) {
-//  if (idtubeN > (tubes.length-1)) {
-//   return;
-//  }
- 
-//  tubes[idtubeN].initDraw();
-//  tubes[idtubeN].callOnFinished = function(){
-  
-//   // console.log(idtubeN,tubes.length);
-//      Snap.select("#tube"+idtubeN)
-//      .attr({stroke : 'rgba(190,215,250,0.85)', filter: glow})
-//      .animate({ 'stroke-width' : 5 },rhythm, function(){idtubeN++; lightTube(idtubeN)})
-//    }
-// }
 
 
 function blincing(obj, N, timing){
   // console.log(N,timing);
   if ( N == 0 ) return;
-  // var that = this;
   N--;
-  obj.animate({ 'stroke' : darckTube }, 20, function ()
+  obj.animate({ 'opacity' : 0.33 }, 50, function ()
        {
-          obj.animate({ 'stroke' : tubeColor }, timing, function() 
-            {
+          obj.animate({ 'opacity' : 1 }, timing, function() 
+            { 
+
               blincing(obj, N, timing)
             }) 
        })
+  // obj.attr({filter : ''}).animate({ 'stroke' : darckTube }, 50, function ()
+  //      {
+  //         obj.animate({ 'stroke' : intermidColor }, timing, function() 
+  //           { 
+  //             setTimeout(
+  //              function() {  obj.attr({filter : glow,  'stroke' : tubeColor});  }
+  //             , timing) ;
+  //             blincing(obj, N, timing)
+  //           }) 
+  //      })
 
 
 }
@@ -65,49 +64,43 @@ function getRandomInt(min, max) {
 }
 
 function newlightTube(idtubeN) {
- if (idtubeN > (tubesPath.length-1)) {
-  // thinWhiteStroce(); 
-  return }
+ if (idtubeN > (tubesPath.length-1)) { return }
  
   // console.log(idtubeN,tubesPath.length);
   var contour =  Snap.select("#tube"+idtubeN);
-    contour.removeClass("neontubes")
+    contour
+    .removeClass("neontubes whiteflag")
      .attr({
             "fill" : 'rgba(250, 250, 255, 1)',
        "fill-rule" : "nonzero",
-       'stroke-width' : 4.5,
+       'stroke-width' : 5,
        "stroke-linejoin" : "round",
        "stroke-linecap" : "round",
        "stroke-dashoffset": 0,
-        stroke : darckTube, 
+        stroke : tubeColor, 
+        filter : '',
         filter : glow,
         opacity : 0.95
         });
      
 
-     if ( getRandomInt(1,4) == 1 ) { blincing(contour, getRandomInt(2,10), getRandomInt(50,2000)); }
+     if ( getRandomInt(1,5) == 1 )
+          { 
+            blincing(contour, getRandomInt(2,10), getRandomInt(50,2000)); 
+          }
       else 
-        {
+        { 
+          contour.animate({ 'stroke' : tubeColor }, rhythm*10, function(){
+            // contour.attr({filter : glow,})
+          }); 
 
-          contour.animate({ 'stroke' : tubeColor }, rhythm*10); 
-          // contour.attr({"fill" : '#ffffff'})
+
+            animSvg.select("#wc"+idtubeN).removeClass('whiteflag') 
         }
-     
-
             idtubeN++; newlightTube(idtubeN);
-// 
 }
  
-// function thinWhiteStroce(){
-//    for (var i in tubesPath)
-//         {
-//            animSvg.add(Snap.parse(tubesPath[i])); 
-//            if ( i == tubesPath.length-1) { animSvg.selectAll('.neontubes').forEach(function(elem){elem.removeClass('neontubes').addClass('neontubesinside');}) } 
-//         }
 
- 
-
-// }
 
 function drawObjects()
     {
@@ -118,75 +111,16 @@ function drawObjects()
       for (var i in tubesPath)
         {
            animSvg.add(Snap.parse(tubesPath[i]));
-           // tubes[i] = new lightning (tubesPath[i], '', 50);
-           // console.log(i);
-           // if ( i == tubesPath.length-1) lightTube(0);
-           // if ( i == tubesPath.length-1) ;
+           animSvg.add(Snap.parse(whitecore[i]));
         }
-        setTimeout(function(){newlightTube(0)}, firsttiming)
+     
+
+        setTimeout(function()
+          {
+            newlightTube(0); 
+          }, firsttiming)
+        
        }
 
-
-
-// function lightning( svgString, transformString, timeBetweenDraws ) {
-//     this.fragment = Snap.parse( svgString );
-//     this.pathArray = this.fragment.selectAll('path');
-//     this.group = animSvg.g().transform( transformString ).drag();
-//     this.timeBetweenDraws = timeBetweenDraws;
-// };
-
-// lightning.prototype.init = function( svgString, transformString ) {
-//       this.group.clear();
-//       this.currentPathIndex = 0;
-
-// };
-
-// lightning.prototype.endReached = function() {
-//     if( this.currentPathIndex >= this.pathArray.length ) {
-//         return true;
-//     };
-// };
-
-// lightning.prototype.callOnFinished = function() {
-// }
-
-// lightning.prototype.initDraw = function() {
-//     this.init();
-//     this.draw();
-// };
-
-// lightning.prototype.quickDraw = function() {
-//     this.init();
-//     this.timeBetweenDraws = 0;
-//     this.draw();
-// };
-
-// lightning.prototype.draw = function() {         // this is the main animation bit
-//     if( this.endReached() ) {
-//         if( this.callOnFinished ) {
-//             this.callOnFinished();
-//             return
-//         };
-//     };
-//     var myPath = this.pathArray[ this.currentPathIndex ] ;
-
-//     this.leng = myPath.getTotalLength();
-
-//     this.group.append( myPath );
-
-//      myPath.attr({
-//        fill: '#fff',
-//        "fill-rule" : "nonzero",
-//        "stroke-linejoin" : "round",
-//        "stroke-linecap" : "round",
-//        "stroke-dasharray": this.leng + " " + this.leng,
-//        "stroke-dashoffset": this.leng
-//      });
-
-//      this.currentPathIndex++;
-
-//      myPath.animate({"stroke-dashoffset": 0}, this.timeBetweenDraws, mina.easeout, this.draw.bind( this ) );
-
-// };
 
 
