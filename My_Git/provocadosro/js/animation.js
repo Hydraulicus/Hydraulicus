45664070cdc71b71d1bde4ef8181de0d930eae5e
zbbrn <852gin@gmail.com>
2016-07-14 11:07:03 +0300

    Add new vertion

--- My_Git/provocadosro/js/animation.js
+++ My_Git/provocadosro/js/animation.js
@@ -1,28 +1,26 @@
 var rhythm = 300//ms
-,hexagonSvg, bottomSvg, cloneFlag = false, group4clone, putclone,
-pathestoicons = [] ,hextext = [] ,textTitles = []
-activeIconAttr = {fill : "rgba(200, 200, 200, 0.01)", opacity : 1, stroke : "gray", "stroke-width" : 2},
-passiveIconAttr = {fill : "#F1F1F1", opacity : 0.7, stroke : "#F1F1F1", "stroke-width" : 0}
-; 
+    ,hexagonSvg, bottomSvg, cloneFlag = false, group4clone, putclone,
+    pathestoicons = [] ,hextext = [] ,textTitles = []
+    activeIconAttr = {fill : "rgba(200, 200, 200, 0.01)", opacity : 1, stroke : "gray", "stroke-width" : 2},
+    passiveIconAttr = {fill : "#F1F1F1", opacity : 0.7, stroke : "#F1F1F1", "stroke-width" : 0}
+    ; 
 
 function initAnimation (obj) {
     hexagonSvg = Snap(obj.hexagon); 
     bottomSvg = Snap(obj.bottombar); 
     rhythm = obj.rhythm * 1000;
-     
 
     drawObjects(hexagonSvg, objects);
-    
     drawObjects(bottomSvg, bottomBar);
+
     insertIconsInBar(bottomSvg, obj.hexagonSegmentsData);
     insertIconsInHex(hexagonSvg, obj.hexagonSegmentsData);
     drawObjects(hexagonSvg, hexcovers);
     bindingHandling();
     buildingCloneLayer();
+    setTextTitlesInHexagon();
     
 };//end of init function
-
-
 
 
 function buildingCloneLayer(){
@@ -32,12 +30,23 @@ function buildingCloneLayer(){
 }
 
 function drawObjects(target_, objects_)
-    { 
+    {
       for (var j in objects_)
         { 
-          target_.add(Snap.parse(objects_[j]))
+          target_.add(Snap.parse(objects_[j]));
+          if  (j ==  objects_.length-1) {   }
         }
    }
+
+setTextTitlesInHexagon = function (){
+  Snap.selectAll(".titles").forEach( function(element) 
+        { var target = element.attr("id").split("-")[0];
+            // console.log(target);
+            // console.log(element.node.textContent);
+            element.node.textContent =  textTitles[target]
+        } 
+    )
+}
 
 function insertIconsInBar(target_, icons4insert)
 { var i=0;
@@ -67,8 +76,6 @@ function insertIconsInHex(target_, icons4insert)
 { var i=0;
   var arrayofIcoPlacess = Snap.selectAll(".bottomiconplaceinhex").forEach( function(element)
         {
-              // console.log(i);
-              // console.log(element.getBBox());
             var icoBox = element.getBBox();
             var  x = icoBox.x + ( icoBox.w - icoBox.r0 ) * 0.5,
                  y = icoBox.y + ( icoBox.h - icoBox.r0 ) * 0.5,
@@ -83,7 +90,6 @@ function insertIconsInHex(target_, icons4insert)
 }
 
 var hoverover = function() { this.attr(activeIconAttr) };
-
 var hoverout = function() { this.attr(passiveIconAttr) };
 
 function bindingHandling(){
@@ -94,32 +100,40 @@ function bindingHandling(){
             .attr({cursor : "pointer"})
             .mouseover(hoveroversegment)
             .mouseout(hoveroutsegment)
-            .click(segmentClick)
-            ;
+            .click(segmentClick);
       })
 }
 
-var segmentClick = function() {
-    //animate to full hexagon
-    cloneFlag = true;
-    var targetId=this.attr("id");
-    // console.log(targetId);
-    group4clone.removeClass("visibility_hid");
+function unbindingHandling(){
+  Snap.selectAll(".hexcover").forEach( function(element) 
+      {
+        // console.log(element);
+        element
+            .attr({cursor : "pointer"})
+            .unmouseover(hoveroversegment)
+            .unmouseout(hoveroutsegment)
+            .unclick(segmentClick);
+      })
+}
 
+function drawHexagonViewMode (targetId) {    //animate to full hexagon
+    group4clone.removeClass("visibility_hid").animate({"opacity" : 1}, rhythm);
     var clone = Snap.select("#"+targetId+"-segment").clone();
-   
-    // console.log(clone.attr("d"));
-    // group4clone.add(clone);
     putclone.add(clone);
     clone
       .stop()
       .animate({"d" : "M468 541c0,0 0,0 0,0 -3,2 -7,3 -10,3l-292 0c-3,0 -6,-1 -9,-2l0 0 0 0c-3,-2 -6,-5 -8,-8l-146 -252c-2,-3 -3,-7 -3,-10l0 0 1 0c0,-3 1,-7 3,-10l145 -252c2,-3 5,-6 8,-8l156 270 0 0 0 0 155 -269c3,2 5,4 7,7l145 252c2,3 3,7 3,10 0,4 -1,7 -3,10l-145 252c-2,3 -4,5 -7,7l0 0zm-155 -269l-156 -270c0,0 0,0 0,0 3,-1 6,-2 9,-2l292 0c3,0 7,1 10,3l-155 269z"}, 150, mina.easiinout)
-      .click(cloneClick);
       ;
-    // // console.log(clone.attr("d"));
+}
 
-    // topiconplace
-     // Snap.select("#circle_"+targetId).attr({fill : "rgba(200, 200, 200, 0.01)", opacity : 1, stroke : "gray", "stroke-width" : 2});
+var segmentClick = function() {
+
+    cloneFlag = true;
+    unbindingHandling();
+    var targetId=this.attr("id");
+    // console.log(targetId);
+    drawHexagonViewMode(targetId);
+
      var icoBox = Snap.select("#topiconplace").getBBox();
             var  x = icoBox.x + ( icoBox.w - icoBox.r0 ) * 0.5,
                  y = icoBox.y + ( icoBox.h - icoBox.r0 ) * 0.5,
@@ -127,14 +141,27 @@ var segmentClick = function() {
             putclone.image( pathestoicons[targetId], x, y, h, h);
       putclone.add(Snap.parse('<text x="320" y="250" fill="white" stroke="white" stroke-width="0.2" font-weight="medium" font-size="30px" font-family="Corbert-Medium" text-anchor="middle">' + textTitles[targetId] + '</text>'));
      var textblock = putclone.multitext(320, 280, hextext[targetId], 355, { "font-size" : "14px", "text-anchor" : "middle", "fill" : "white", "stroke" : "white", "stroke-width" : "0.2", "font-weight" : "normal", "font-family" : "Corbert",  });
+
+     Snap.select("#closecross").attr({"cursor" : "pointer"}).click( cloneClick );
+     Snap.select("#arrowright").attr({"cursor" : "pointer"}).click( function() { changeInfoIntoHexagonViewMode("pp") });
+     Snap.select("#arrowleft").attr({"cursor" : "pointer"}).click( function() { changeInfoIntoHexagonViewMode("rs") } );
 }
 
-var cloneClick = function () {
-  // console.log("cloneClick");
-  this.animate({"opacity" : 0}, rhythm, function(){cloneFlag = false; group4clone.addClass("visibility_hid"); putclone.clear(); collapseAllSegment (); });
+var cloneClick = function (callback) {//close view mode of hexagon
+  group4clone.animate({"opacity" : 0}, rhythm, function(){cloneFlag = false; group4clone.addClass("visibility_hid"); putclone.clear(); collapseAllSegment (); bindingHandling() });
   Snap.selectAll(".icoInBar").forEach( function (element) { element.attr(passiveIconAttr); } ) ;
-  
 }
+
+changeInfoIntoHexagonViewMode = function(par){
+   console.log("changeInfoIntoHexagonViewMode",par); 
+ group4clone.animate({"opacity" : 0}, rhythm, function(){cloneFlag = false; group4clone.addClass("visibility_hid"); putclone.clear(); collapseAllSegment (); drawHexagonViewMode (par) });
+  Snap.selectAll(".icoInBar").forEach( function (element) { element.attr(passiveIconAttr); } ) ;
+} 
+
+autoOpeningInformationMode = function () {
+  console.log("autoOpeningInformationMode");
+}
+
 
 function hoveroversegment () { 
       var that = this,
@@ -167,12 +194,9 @@ function collapseAllSegment ()
     {
      Snap.selectAll(".segments").forEach( function (element) { 
 
-        // Snap.select("#"+targetId+"-segment");
         var targetId = element.attr("id");
-        // console.log(targetId.split("-"));
         if (targetId.split("-")[0] == "mr") //it need becous mr have bug in contour animation
             { 
-              // console.log( targetId.split("-")[0]);
               element.stop().attr({d : initialsegmentcontours[targetId.split("-")[0]]}); 
               return
             }
