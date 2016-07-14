198e5209f6702529aacac06bcff7d2f615398d62
zbbrn <852gin@gmail.com>
2016-07-14 08:53:01 +0300

    Add new vertion

--- My_Git/provocadosro/js/animation.js
+++ My_Git/provocadosro/js/animation.js
@@ -1,6 +1,8 @@
 var rhythm = 300//ms
 ,hexagonSvg, bottomSvg, cloneFlag = false, group4clone, putclone,
 pathestoicons = [] ,hextext = [] ,textTitles = []
+activeIconAttr = {fill : "rgba(200, 200, 200, 0.01)", opacity : 1, stroke : "gray", "stroke-width" : 2},
+passiveIconAttr = {fill : "#F1F1F1", opacity : 0.7, stroke : "#F1F1F1", "stroke-width" : 0}
 ; 
 
 function initAnimation (obj) {
@@ -51,6 +53,7 @@ function insertIconsInBar(target_, icons4insert)
             target_.circle(icoBox.cx, icoBox.cy, icoBox.r1 * 1.2) // cover circle. need for bind events
                     .attr({fill : "#F1F1F1", opacity : 0.7, stroke : "black", "stroke-width" : 0, cursor : "pointer"})
                     .attr({"id" : "circle_" + icons4insert[i]["name"]})
+                    .addClass("icoInBar")
                     .hover(hoverover, hoverout)
                     ;
             pathestoicons[icons4insert[i]["name"]] = icons4insert[i]["pathToIco"];
@@ -79,8 +82,9 @@ function insertIconsInHex(target_, icons4insert)
         })
 }
 
-var hoverover = function() { this.attr({fill : "rgba(200, 200, 200, 0.01)", opacity : 1, stroke : "gray", "stroke-width" : 2}) };
-var hoverout = function() { this.attr({fill : "#F1F1F1", opacity : 0.7, stroke : "#F1F1F1", "stroke-width" : 0}) };
+var hoverover = function() { this.attr(activeIconAttr) };
+
+var hoverout = function() { this.attr(passiveIconAttr) };
 
 function bindingHandling(){
   Snap.selectAll(".hexcover").forEach( function(element) 
@@ -121,14 +125,15 @@ var segmentClick = function() {
                  y = icoBox.y + ( icoBox.h - icoBox.r0 ) * 0.5,
                  h = icoBox.r0;
             putclone.image( pathestoicons[targetId], x, y, h, h);
-     // var titlrtext = putclone.Snap.parse('<text x="320" y="250" fill="white" stroke="white" stroke-width="0.2" font-weight="bold" font-size="18.8px" font-family="Corbert-ExtraBold" text-anchor="middle" ">' + '</text>');
       putclone.add(Snap.parse('<text x="320" y="250" fill="white" stroke="white" stroke-width="0.2" font-weight="medium" font-size="30px" font-family="Corbert-Medium" text-anchor="middle">' + textTitles[targetId] + '</text>'));
      var textblock = putclone.multitext(320, 280, hextext[targetId], 355, { "font-size" : "14px", "text-anchor" : "middle", "fill" : "white", "stroke" : "white", "stroke-width" : "0.2", "font-weight" : "normal", "font-family" : "Corbert",  });
 }
 
 var cloneClick = function () {
   // console.log("cloneClick");
-  this.animate({"opacity" : 0}, rhythm, function(){cloneFlag = false; group4clone.addClass("visibility_hid"); putclone.clear(); })
+  this.animate({"opacity" : 0}, rhythm, function(){cloneFlag = false; group4clone.addClass("visibility_hid"); putclone.clear(); collapseAllSegment (); });
+  Snap.selectAll(".icoInBar").forEach( function (element) { element.attr(passiveIconAttr); } ) ;
+  
 }
 
 function hoveroversegment () { 
@@ -138,7 +143,7 @@ function hoveroversegment () {
       // console.log( targetId );
             // Snap.select("#mr-segment").attr({d : segmentcontours["mr"]}); 
       target.stop().animate({d : segmentcontours[targetId]}, rhythm*2, mina.easinout);
-      Snap.select("#circle_"+targetId).attr({fill : "rgba(200, 200, 200, 0.01)", opacity : 1, stroke : "gray", "stroke-width" : 2});
+      Snap.select("#circle_"+targetId).attr(activeIconAttr);
   };
 
  function hoveroutsegment() { 
@@ -152,10 +157,15 @@ function hoveroversegment () {
       // console.log("hoveroutsegment",target);
       // console.dir(Snap.selectAll(".segments"));
       // target.attr({d : initialsegmentcontours[targetId]});
-      Snap.select("#circle_"+targetId).attr({fill : "#F1F1F1", opacity : 0.7, stroke : "#F1F1F1", "stroke-width" : 0});
+      Snap.select("#circle_"+targetId).attr(passiveIconAttr);
       // console.log( Snap.select("#mr"));
       // Snap.select("#mr").attr({d : initialsegmentcontours["mr"]}); 
-      Snap.selectAll(".segments").forEach( function (element) { 
+      collapseAllSegment ();
+  };
+
+function collapseAllSegment ()
+    {
+     Snap.selectAll(".segments").forEach( function (element) { 
 
         // Snap.select("#"+targetId+"-segment");
         var targetId = element.attr("id");
@@ -167,5 +177,5 @@ function hoveroversegment () {
               return
             }
         element.stop().animate({d : initialsegmentcontours[targetId.split("-")[0]]}, rhythm, mina.easinout); 
-      }) 
-  };
+      })  
+    }
