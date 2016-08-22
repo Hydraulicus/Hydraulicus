d8ae1fdbc92fcc32edb855c23741a6df3142b2a8
zbbrn <852gin@gmail.com>
2016-08-22 21:09:05 +0300

    Add new vertion

--- My_Git/textscaling/js/animation.js
+++ My_Git/textscaling/js/animation.js
@@ -5,9 +5,17 @@ function initial (obj) {
     var targetTopTxt = document.getElementById('toptext');
     var targetBottomTxt = document.getElementById('bottomtext');
     inputTopText.onkeyup = function() {
-        targetTopTxt.textContent = this.value;
+        var txt = this.value;
+        var textFactor = (txt.length >= 6) ? 6/txt.length : 1;
+        targetTopTxt.textContent = txt;
+        //targetTopTxt.setAttribute("font-size", 85*textFactor+"px");
+        document.getElementById('toptext').style.fontSize = 85*textFactor+"px";
     };
     inputBottomText.onkeyup = function() {
-        targetBottomTxt.textContent = this.value;
+        var txt = this.value;
+        var textFactor = (txt.length >= 12) ? 12/txt.length : 1;
+        targetBottomTxt.textContent = txt;
+        //document.getElementById('toptext4changesize').setAttribute("font-size", 38*textFactor+"px")
+        document.getElementById('bottomtext').style.fontSize = 38*textFactor+"px";
     };
 };//end of init function
