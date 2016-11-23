c12c2f639fae9bde5246fb57a9986dac45c1259b
zbbrn <852gin@gmail.com>
2016-11-23 10:48:02 +0200

    Add new vertion

--- My_Git/dubai/js/animation.js
+++ My_Git/dubai/js/animation.js
@@ -1,15 +1,22 @@
-var pathTag, s, animTime = [];
+var animTime = [], closeAnimTime = [];
 function initAnimation (obj) {
-    animTime = obj.AnimTime;
-    embdingSvgFileByAJAX ( obj.svgWrap , obj.pathTosvgFile, performOfImage);
+
+    animTime = obj[0].AnimTime;
+    embdingSvgFileByAJAX ( obj[0].svgWrap , obj[0].pathTosvgFile, function() { initCloseUp (obj[1]); if ( obj[0].animated)  performOfWhallMap ()});
 };//end of init function
+
+function initCloseUp (obj) {
+    closeAnimTime = obj.AnimTime;
+    embdingSvgFileByAJAX ( obj.svgWrap , obj.pathTosvgFile, function() { if ( obj.animated)  performOfCloseMap ()});
+}
 
 function embdingSvgFileByAJAX (target_tag, filePathName, callBack) {
     var target_ = document.querySelector( target_tag );
     var ajax = new XMLHttpRequest();
+    var pathTag;
     ajax.open("GET", filePathName, true);
     ajax.send();
-    ajax.onload = function(e) {
+    ajax.onload = function() {
         target_.innerHTML = ajax.responseText;
         pathTag = Snap.selectAll(target_tag + " .animationstages");
         pathTag.forEach( function(elelment) {
@@ -19,12 +26,36 @@ function embdingSvgFileByAJAX (target_tag, filePathName, callBack) {
         };
 }
 
-function performOfImage () {
-    // id="track2" class="animationstages animationstage2"
-    s = Snap("#maps4anim");
-    var p1 = new Drawing( "#track1", "", animTime[0] );
-    var p2 = new Drawing( "#track2", "", animTime[1] );
-    var p3 = new Drawing( "#track3", "", animTime[2] );
+function performOfCloseMap () {
+    var s = Snap("#closemap");
+    var p1 = new Drawing( "#track1_1", "", closeAnimTime[0], s, 0.5 );
+    var p2 = new Drawing( "#track1_2", "", closeAnimTime[1], s, 0.5 );
+    var p3 = new Drawing( "#track1_3", "", closeAnimTime[2], s, 0.5 );
+    var p4 = new Drawing( "#track1_4", "", closeAnimTime[3], s, 0.5 );
+    p1.initDraw();
+    p1.callOnFinished = function() {
+        dotting("#track1_1");
+        p2.initDraw();
+    };
+
+    p2.callOnFinished = function() {
+        dotting("#track1_2");
+        p3.initDraw();
+    };
+    p3.callOnFinished = function() {
+        dotting("#track1_3");
+        p4.initDraw();
+    };
+    p4.callOnFinished = function() {
+        dotting("#track1_4");
+    }
+}
+
+function performOfWhallMap () {
+    var s = Snap("#maps4anim");
+    var p1 = new Drawing( "#track1", "", animTime[0], s );
+    var p2 = new Drawing( "#track2", "", animTime[1], s );
+    var p3 = new Drawing( "#track3", "", animTime[2], s );
 
     p1.initDraw();
     setTimeout(function(){ Snap.selectAll(".animationstage1_5").forEach( function (element) {element.attr({"opacity" : 1}) })}, animTime[0] *0.75 );
@@ -46,12 +77,19 @@ function performOfImage () {
         Snap.selectAll(".animationstage3").forEach( function (element) {element.attr({"opacity" : 1})}  );
         setTimeout(function(){
                 var target = Snap.select("#pin");
-                //target.transform("s0.01 t0,120").attr({opacity: 1});
                 target.animate({transform: "s1.25 t0,-25"}, 500, mina.easeout(), function(){
                     target.animate({transform: "s1 t0,0"}, 500, mina.elastic);
                 });
             }, animTime[2] + 200 );
     };
+}
+
+function dotting(Id, canva){
+    var target = Snap.select(Id);
+    target.attr({
+        opacity: 1,
+        "stroke-dasharray": 1 + " " + 12
+    });
 }
 
 function updateFig(target, textVol, timing){
@@ -66,7 +104,8 @@ console.log(target);
     }, timing);
 }
 
-function Drawing( svgString, transformString, timeBetweenDraws ) {
+function Drawing( svgString, transformString, timeBetweenDraws, s, opacity ) {
+    this.opacity = opacity || 1;
     //this.fragment = Snap.parse( svgString );
     this.fragment = s.select( svgString );
     this.pathArray = [this.fragment];
@@ -115,14 +154,14 @@ Drawing.prototype.draw = function() {         // this is the main animation bit
 
     myPath.attr({
         fill: 'none',
-        opacity: 1,
+        opacity:this.opacity,
         "stroke-dasharray": this.leng + " " + this.leng,
         "stroke-dashoffset": this.leng
     });
 
     this.currentPathIndex++;
 
-    myPath.animate({"stroke-dashoffset": 0}, this.timeBetweenDraws, mina.easeout, this.draw.bind( this ) );
+    myPath.animate({"stroke-dashoffset": 0}, this.timeBetweenDraws, mina.easeinout, this.draw.bind( this ) );
 
 };
 
