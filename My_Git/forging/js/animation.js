0b9b5cbcf6c46300a737c301ce4da5516823ec2e
Alexei Zababurin <azababurin@finsystems.ltd>
2018-10-17 00:29:38 +0300

    Add new vertion

--- My_Git/forging/js/animation.js
+++ My_Git/forging/js/animation.js
@@ -1,4 +1,12 @@
+function stopAnimation() {
+	// [].forEach.call(document.getElementsByClassName("bricks"), function (el) {el.setAttribute("class","disappear");});
 
-function initAnimation (obj) {
-    rhythm = obj.rhythm * 1000 || 300; //ms
-};//end of init function
+	[].forEach.call(document.getElementsByClassName("st0"), function (el) {el.setAttribute("class","st0");});
+	[].forEach.call(document.getElementsByClassName("st1"), function (el) {el.setAttribute("class","st1");});
+
+	const tempBricks = document.getElementById("tempBricks");
+	tempBricks.parentNode.removeChild(tempBricks);
+
+	document.getElementById("brick1").setAttribute("class","st0");
+	document.getElementById("forgingBlock").setAttribute("class","forging-block-normal-size");
+};
