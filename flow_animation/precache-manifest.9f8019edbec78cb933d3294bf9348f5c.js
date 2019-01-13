67dfad56a4574c98c535a1eeb65727e5ca6768b3
Alexei Zababurin <852gin@gmail.com>
2019-01-13 17:17:16 +0200

    Add new vertion

---
+++ flow_animation/precache-manifest.9f8019edbec78cb933d3294bf9348f5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7ecf20df02f1455c9ee6691cd7ab707",
+    "url": "/static/media/pipe_rear.c7ecf20d.svg"
+  },
+  {
+    "revision": "476087bf08debb3fc8b24ab588e2a38d",
+    "url": "/static/media/pipe_front.476087bf.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3979b1475e7ede54e2c",
+    "url": "/static/js/main.e3979b14.chunk.js"
+  },
+  {
+    "revision": "6544fe970ae5cf82e66e",
+    "url": "/static/js/1.6544fe97.chunk.js"
+  },
+  {
+    "revision": "e3979b1475e7ede54e2c",
+    "url": "/static/css/main.18f866bd.chunk.css"
+  },
+  {
+    "revision": "88cc4545630c6de3b04c718cf4fde381",
+    "url": "/index.html"
+  }
+];
